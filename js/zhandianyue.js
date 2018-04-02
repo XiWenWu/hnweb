c2ee3ae00ca7bf76c515abf74dbdfd3c6da07c99
wuxiwen <279712908@qq.com>
2018-04-02 17:03:01 +0800

    add

--- js/zhandianyue.js
+++ js/zhandianyue.js
@@ -19,6 +19,23 @@ var zdye=new Vue({
       {nav:"维护日历",url:"weihurili.html"}
     ],
     allStYe:[],
+    // 饼图按钮
+    siteBtn:[
+      {name:"水位", active:true},
+      {name:"图像", active:false},
+      {name:"雨量", active:false},
+      {name:"预警", active:false},
+    ],
+    // 饼图水位数据
+    siteDataSW:[],
+    // 饼图图像数据
+    siteDataTX:[],
+    // 饼图雨量数据
+    siteDataYL:[],
+    // 饼图预警数据
+    siteDataYJ:[],
+    allProvince:[],
+    yueColumn:[[],[],[],[]],
     url:"/svrapi/stmgr/getAllStYe"
   },
   // 
@@ -29,49 +46,6 @@ var zdye=new Vue({
   updated(){
     // 页面加载后执行
     window.parent.document.getElementById("rightIframe").style.height=document.body.offsetHeight+20+"px";
-    // 生成echarts图像  余额站点
-    var yueSite=echarts.init(document.getElementById("yue-site"));
-    yueSite.setOption({
-      tooltip : {
-        trigger: 'item',
-        formatter: "{a} {b}<br/> {c} ({d}%)"
-      },
-      series : [
-        {
-          name: '服务器',
-          type: 'pie',
-          radius : '60%',
-          center: ['50%', '50%'],
-          data:[
-            {value:10, name:'正常'},
-            {value:4, name:'异常'},
-            {value:1, name:'闲置'}
-          ],
-          itemStyle: {
-            emphasis: {
-              shadowBlur: 10,
-              shadowOffsetX: 0,
-              shadowColor: 'rgba(0, 0, 0, 0.5)'
-            }
-          },
-          label: {
-            normal: {
-              show: true,
-              position: "inside",
-              formatter: "{b}\n{c}({d}%)",
-              textStyle: {
-                  color: '#ffffff',
-              }
-            }
-          }
-        }
-      ],
-      color: [
-        '#7dccb1',
-        '#d60015',
-        '#9f9f9f'
-      ]
-    })
   },
   methods:{
     getAllServerStatus:function(){
@@ -80,6 +54,14 @@ var zdye=new Vue({
         this.allStYe=response.body.sort(strSort);
         // 增加总数
         this.setAllStYeTotal();
+        // 设置饼图的数据
+        this.setPieSiteDatas();
+        // 根据数据绘出饼图
+        this.drawSitePie(this.siteDataSW);
+        // 设置柱状图数据
+        this.setColumnDates();
+        // 
+        this.drawColumnPie();
       })
       .catch(function(response){
         console.log(response)
@@ -112,6 +94,165 @@ var zdye=new Vue({
 
         var totalJ=item["预警站"]["超50"]+item["预警站"]["超20"]+item["预警站"]["不足20"]+item["预警站"]["欠20"]+item["预警站"]["欠超20"];
         Vue.set(item["预警站"],"total",totalJ);
+      })
+    },
+    // 改变站点的显示
+    setPieSiteDatas:function(){
+      var sw=[{value:0, name:'欠费数'},{value:0, name:'缴费数'}];
+      var tx=[{value:0, name:'欠费数'},{value:0, name:'缴费数'}];
+      var yl=[{value:0, name:'欠费数'},{value:0, name:'缴费数'}];
+      var yj=[{value:0, name:'欠费数'},{value:0, name:'缴费数'}];
+      var _this=this;
+      _this.allStYe.forEach(function(item, index){
+        sw[0].value+=(item["水位站"]["欠20"]+item["水位站"]["欠超20"]);
+        sw[1].value+=(item["水位站"]["超50"]+item["水位站"]["超20"]+item["水位站"]["不足20"]);
+        tx[0].value+=(item["图像站"]["欠20"]+item["图像站"]["欠超20"]);
+        tx[1].value+=(item["图像站"]["超50"]+item["图像站"]["超20"]+item["图像站"]["不足20"]);
+        yl[0].value+=(item["雨量站"]["欠20"]+item["雨量站"]["欠超20"]);
+        yl[1].value+=(item["雨量站"]["超50"]+item["雨量站"]["超20"]+item["雨量站"]["不足20"]);
+        yj[0].value+=(item["预警站"]["欠20"]+item["水位站"]["欠超20"]);
+        yj[1].value+=(item["预警站"]["超50"]+item["预警站"]["超20"]+item["预警站"]["不足20"]);
+      });
+      _this.siteDataSW=sw;
+      _this.siteDataTX=tx;
+      _this.siteDataYL=yl;
+      _this.siteDataYJ=yj;
+    },
+    // 点击后改变饼图信息
+    changeSiteData:function(index){
+      var _this=this;
+      switch(index){
+        case 0:
+          _this.drawSitePie(_this.siteDataSW);
+          _this.siteBtn.forEach(function(item){item.active=false})
+          _this.siteBtn[0].active=true;
+          break;
+        case 1:
+          _this.drawSitePie(_this.siteDataTX);
+          _this.siteBtn.forEach(function(item){item.active=false})
+          _this.siteBtn[1].active=true;
+          break;
+        case 2:
+          _this.drawSitePie(_this.siteDataYL);
+          _this.siteBtn.forEach(function(item){item.active=false})
+          _this.siteBtn[2].active=true;
+          break;
+        case 3:
+          _this.drawSitePie(_this.siteDataYJ);  
+          _this.siteBtn.forEach(function(item){item.active=false})
+          _this.siteBtn[3].active=true;
+          break;
+      }
+    },
+    // 
+    drawSitePie:function(data){
+      // 生成echarts图像  余额站点
+      var yueSite=echarts.init(document.getElementById("yue-site"));
+      yueSite.setOption({
+        tooltip : {
+          trigger: 'item',
+          formatter: "{a} {b}<br/> {c} ({d}%)"
+        },
+        series : [
+          {
+            name: '服务器',
+            type: 'pie',
+            radius : '60%',
+            center: ['50%', '50%'],
+            data:data,
+            itemStyle: {
+              emphasis: {
+                shadowBlur: 10,
+                shadowOffsetX: 0,
+                shadowColor: 'rgba(0, 0, 0, 0.5)'
+              }
+            },
+            label: {
+              normal: {
+                show: true,
+                position: "inside",
+                formatter: "{b}\n{c}({d}%)",
+                textStyle: {
+                    color: '#ffffff',
+                }
+              }
+            }
+          }
+        ],
+        color: [
+          '#d60015',
+          '#7dccb1'
+        ]
+      })
+    },
+    // 设置柱状图数据
+    setColumnDates:function(){
+      var _this=this;
+      _this.allStYe.forEach(function(item, index){
+        _this.allProvince.push(item.adnm);
+        _this.yueColumn[0].push(item["水位站"]["欠20"]+item["水位站"]["欠超20"]);
+        _this.yueColumn[1].push(item["图像站"]["欠20"]+item["图像站"]["欠超20"]);
+        _this.yueColumn[2].push(item["雨量站"]["欠20"]+item["雨量站"]["欠超20"]);
+        _this.yueColumn[3].push(item["预警站"]["欠20"]+item["预警站"]["欠超20"]);
+      })
+    },
+    // 绘制柱状图
+    drawColumnPie:function(){
+      var yueColumn=echarts.init(document.getElementById("yue-column"));
+      yueColumn.setOption({
+        tooltip: {
+          trigger: 'axis',
+          axisPointer : {            // 坐标轴指示器，坐标轴触发有效
+            type : 'shadow',        // 默认为直线，可选为：'line' | 'shadow'
+          }
+        },
+        legend: {
+            data:['水位站','图像站','雨量站','预警站']
+        },
+        grid:{
+          top:50,
+          right:40,
+          bottom:30,
+          left:40
+        },
+        xAxis :[
+          {
+            type : 'category',
+            data : this.allProvince,
+            axisLabel: {
+              interval: 0,
+              rotate:-30
+            },
+          }
+        ],
+        yAxis : {
+          type : 'value',
+          minInterval : 1,
+          min: 0,
+          name: "次数"
+        },
+        series : [
+          {
+            name:'水位站',
+            type:'bar',
+            data:this.yueColumn[0]
+          },
+          {
+            name:'图像站',
+            type:'bar',
+            data:this.yueColumn[1]
+          },
+          {
+            name:'雨量站',
+            type:'bar',
+            data:this.yueColumn[2]
+          },
+          {
+            name:'预警站',
+            type:'bar',
+            data:this.yueColumn[3]
+          }
+        ]
       })
     }
   },
